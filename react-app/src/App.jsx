aa0f3216e524eb6b42ec86594b153f9fe3531d9e
Deeptesh <deepteshpolji007@gmail.com>
2022-10-11 19:58:53 +0530

    added charts

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,37 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NewExpense } from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
-function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Car Insurance',
-      amount: '98.67',
-      date: new Date(2022, 9, 30),
-    },
-    {
-      id: 'e2',
-      title: 'Bike Insurance',
-      amount: '998.67',
-      date: new Date(2022, 9, 30),
-    },
-    {
-      id: 'e3',
-      title: 'Life Insurance',
-      amount: '233.67',
-      date: new Date(2022, 9, 30),
-    },
-    {
-      id: 'e4',
-      title: 'Health Insurance',
-      amount: '9688.67',
-      date: new Date(2022, 9, 30),
-    },
-  ];
 
+const dummyExpenses = [
+  {
+    id: 'e1',
+    title: 'Car Insurance',
+    amount: '98.67',
+    date: new Date(2022, 9, 30),
+  },
+  {
+    id: 'e2',
+    title: 'Bike Insurance',
+    amount: '998.67',
+    date: new Date(2022, 9, 30),
+  },
+  {
+    id: 'e3',
+    title: 'Life Insurance',
+    amount: '233.67',
+    date: new Date(2022, 9, 30),
+  },
+  {
+    id: 'e4',
+    title: 'Health Insurance',
+    amount: '9688.67',
+    date: new Date(2022, 9, 30),
+  },
+];
+const App = () => {
+  const [expenses, setExpenses] = useState(dummyExpenses);
   const addExpenseHandler = (expense) => {
-    console.log('appp');
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
   return (
     <div>
@@ -39,6 +41,6 @@ function App() {
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
